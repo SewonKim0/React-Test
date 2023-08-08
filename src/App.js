c317c6179c1369b71c0b936386647338d1c845a1
Sewon Kim <sewonkim0black@gmail.com>
2023-08-07 23:28:53 -0400

    Routing error fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import './App.css';
+import { Link } from "react-router-dom"
 
 export default function App() {
-  return <p> Hello world! </p>
+  return <div>
+    <p> App </p>
+    <Link to="/"> App </Link>
+    <br />
+    <Link to="/test"> Test </Link>
+  </div>
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Routes, Route } from "react-router-dom"
+import App from './App'
+import Test from "./Test"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const hashRouter = (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
+  </HashRouter>
+)
+
+ReactDOM.render(hashRouter, document.getElementById("root"))
